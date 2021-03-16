d1533530b36664ecf6032a8f2d75da80403d5990
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2021-03-16 19:53:38 +0600

    enemy falls down

--- Assets/2D-Platformer-Hunter/Scripts/2D/Enemies/Enemy.cs
+++ Assets/2D-Platformer-Hunter/Scripts/2D/Enemies/Enemy.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, IDamagable
 {
 
+    [SerializeField]
+    private int _amplitude = 1;
+    [SerializeField]
+    private float _frequency = 2.5f;
+
+    // initial positin by X axis
+    private float xPos;
+
 	public float Health { get; }
 	public void TakeDamage (float damage){
         Debug.Log("damge taken");
         Debug.Log(damage);
         Destroy(gameObject);
     }
+
+    void Start() {
+        xPos = transform.position.x;
+    }
+
+    void Update () {
+
+        Vector3 curPosition = transform.position;
+        curPosition.x = xPos + Mathf.Cos(Time.time * _frequency) * _amplitude;
+
+        // Debug.Log(curPosition);
+
+        transform.position = curPosition;
+    }
+
 }
